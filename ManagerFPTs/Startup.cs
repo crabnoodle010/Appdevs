0543ec6d681c0147ecfef0fa79464497b2319d59
Noodle Crab <ducpvmgcd17077@fpt.edu.vn>
2021-06-21 16:02:56 +0700

    Add project files.

---
+++ ManagerFPTs/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ManagerFPTs.Startup))]
+namespace ManagerFPTs
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
