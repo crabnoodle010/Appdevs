b4b29bf1853129f6c1b6427a508fcd47b65f4fac
Noodle Crab <ducpvmgcd17077@fpt.edu.vn>
2021-06-22 19:05:25 +0700

    Add trainer

---
+++ ManagerFPTs/Controllers/TrainerIfsController.cs
@@ -0,0 +1,25 @@
+﻿using ManagerFPTs.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ManagerFPTs.Controllers
+{
+    public class TrainerIfsController : Controller
+    {
+        private ApplicationDbContext _context;
+        public TrainerIfsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: TrainerIfs
+        public ActionResult Index()
+        {
+            var trainerIfs = _context.UserIfs.OfType<TrainerIf>().ToList();
+            return View(trainerIfs);
+        }
+    }
+}
---
+++ ManagerFPTs/Models/TrainerIf.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ManagerFPTs.Models
+{
+    public class TrainerIf : UserIf
+    {
+        public string WorkingPlace { get; set; }
+    }
+}
